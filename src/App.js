11974581ce6e6457a30cd5ee71d0520cf91a1f32
Murali <muralivvrsn75683@gmail.com>
2022-12-24 17:27:49 +0530

    redux installed and counter app created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import store from './store';
 
 function App() {
+  console.log(store);
+  const counter = useSelector((state)=>state.counter);
+  const dispatch = useDispatch();
+  const increment = ()=>{
+    dispatch({type:"INC"});
+  }
+  const decrement = ()=>{
+    dispatch({type:"DEC"});
+  }
+  console.log(counter)
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <h1>Counter App</h1>
+      <h2>{counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import {createStore} from 'redux';
+
+const reducerFn = (state ={counter:0}, action) =>{
+    // Synchronous Function
+    // We should not mutate the original state
+    if(action.type==="INC"){
+        return {counter:state.counter+1};
+    }
+    if(action.type==="DEC"){
+        return {counter:state.counter-1};
+    }
+    return state;
+}
+const store = createStore(reducerFn);
+export default store;
